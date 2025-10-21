3f518e395751e1de0f3d0ae3893222e63a6ed72b
wsinned <wsinned@users.noreply.github.com>
2025-10-21 14:00:15 +0100

    Corrects typo

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ ln -sf "$DOTFILES_REPOSITORY/.config/fish" "$HOME/.config/fish"
 ln -sf "$DOTFILES_REPOSITORY/.config/atuin" "$HOME/.config/atuin"
 
 # Create symlinks for bash configuration
-ln -sf "$DOTFILES_REPOSITORY/.config/starship.toml" "$HOME/.config/staship.toml"
+ln -sf "$DOTFILES_REPOSITORY/.config/starship.toml" "$HOME/.config/starship.toml"
 
 
 echo "Dotfiles installation complete!"
