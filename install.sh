58e2a2b7301a5f52f603ca97d5edf898227f0edc
wsinned <wsinned@users.noreply.github.com>
2025-10-21 12:22:53 +0100

    Removes env var from install script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create symlinks for bash configuration
-ln -sf "$DOTFILES_REPOSITORY/.config/fish" "$HOME/.config/fish"
+ln -sf ".config/fish" "$HOME/.config/fish"
 
 # Create symlinks for bash configuration
-ln -sf "$DOTFILES_REPOSITORY/.config/atuin" "$HOME/.config/atuin"
+ln -sf ".config/atuin" "$HOME/.config/atuin"
 
 # Create symlinks for bash configuration
-ln -sf "$DOTFILES_REPOSITORY/.config/starship.toml" "$HOME/.config/staship.toml"
+ln -sf "/.config/starship.toml" "$HOME/.config/staship.toml"
 
 
 echo "Dotfiles installation complete!"
